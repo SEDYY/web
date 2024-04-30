a84b93a43321e31e46abd3e05dfb88e2fbee4b49
Sedy <sosiskaed228@gmail.com>
2024-04-30 23:45:07 +0300

    первый комит епрст

---
+++ menu.js
@@ -0,0 +1,10 @@
+/* Установить ширину боковой панели с шириной 250 пикселей и следующий */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Установите ширину боковой навигации в 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
